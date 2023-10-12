1890a60546a939266def49a9e3bff59b4a1968c7
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2023-10-13 00:45:48 +0100

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+function changeTheme(them) {
+   if (them == 'dark') {
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+   }
+   else {
+      document.body.style.backgroundColor = '';
+      document.body.style.color = '';
+   }
+   
+}
